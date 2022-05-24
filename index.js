f1b474d1b78fd0eb565dffcb7779ff1b321f9203
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-24 16:29:59 +0600

    get users api added

--- index.js
+++ index.js
@@ -14,23 +14,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.zz02t.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: 'Unauthorized access'})
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN, function(err,  decoded){
-        if(err){
-            return res.status(403).send({message: 'forbidden access'})
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
         }
         req.decoded = decoded;
+        // console.log('inside verifyJWT', decoded)
         next();
     })
 }
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
 
         const reviewsCollection = client.db('manufacturer-website').collection('reviews');
@@ -39,31 +40,44 @@ async function run(){
         const usersCollection = client.db('manufacturer-website').collection('users');
 
         // get
-        app.get('/reviews', async(req, res) => {
+        app.get('/reviews', async (req, res) => {
             const query = {};
             const result = await reviewsCollection.find(query).toArray();
             res.send(result);
         })
-        app.get('/products', async(req,res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
             const result = await productsCollection.find(query).toArray();
             res.send(result);
         })
-        app.get('/product/:id', async(req,res) => {
+        app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await productsCollection.findOne(query);
             res.send(result)
         })
+        app.get('/users', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester, })
+            if (requesterAccount.role === 'Admin') {
+                const query = {};
+                const result = await usersCollection.find(query).toArray();
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
+        })
 
         // post
-        app.post('/review', async(req, res) => {
+        app.post('/review', async (req, res) => {
             const data = req.body;
             const doc = data;
             const result = await reviewsCollection.insertOne(doc);
             res.send(result)
         })
-        app.post('/order', async(req, res) => {
+        app.post('/order', verifyJWT, async (req, res) => {
             const data = req.body;
             const doc = data;
             const result = await ordersCollection.insertOne(doc);
@@ -71,24 +85,24 @@ async function run(){
         })
 
         // put
-        app.put('/user/:email', async(req, res) => {
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            const filter = {email: email};
+            const filter = { email: email };
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
             }
             const result = await usersCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN, {
+            const accessToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
                 expiresIn: '1d'
             })
-            res.send({result, token});
+            res.send({ result, accessToken });
         })
 
         console.log('Database connected')
     }
-    finally{
+    finally {
 
     }
 }
